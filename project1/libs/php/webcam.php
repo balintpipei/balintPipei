bd24e8838e973e456b1482dca119e5b589df6832
balintpipei <81591626+balintpipei@users.noreply.github.com>
2022-03-10 13:29:42 +0000

    update php filles 2.0

---
+++ project1/libs/php/webcam.php
@@ -0,0 +1,34 @@
+<?php
+
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    $executionStartTime = microtime(true);
+
+    $url='https://api.windy.com/api/webcams/v2/list/country='. $_REQUEST['countryCode'] . '?show=webcams:image,location,map,player,url&lang=en';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'x-windy-key: bjPbAl093xUt2jALC91eqDjpuEEAHkHX'
+    ));
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $result = curl_exec($ch);
+
+    curl_close($ch);
+
+    $decode= json_decode($result, true);
+
+    $output['status']['code'] = '200';
+    $output['status']['name'] = 'ok';
+    $output['status']['description'] = 'success';
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . ' ms';
+    $output['data'] = $decode;
+
+        
+    header('Content-Type: application/json; charset=UTF-8');
+    echo json_encode($output);
+
+?>
