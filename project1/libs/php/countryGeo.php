e3bee341a74310d5a75bb423b2561cb91c0f2626
balintpipei <81591626+balintpipei@users.noreply.github.com>
2022-03-13 15:39:25 +0000

    updated php

--- project1/libs/php/countryGeo.php
+++ project1/libs/php/countryGeo.php
@@ -1,5 +1,6 @@
 <?php
  
+    // Echo all errors back to the screen of the browser so PHP can be debugged
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
     ini_set('memory_limit', '1024M');
@@ -9,29 +10,19 @@
     $countryData = json_decode(file_get_contents("../json/countryBorders.geo.json"), true);
 
     // Create an empty array to add data to
-    $country = [];
+    $border = [];
 
+    // Use a for each loop to go through each feature of the geoJson data and get the country name and ISO 3 code
     foreach ($countryData['features'] as $feature) {
 
-         $temp = null;
-         $temp['code'] = $feature["properties"]['iso_a2'];
-         $temp['name'] = $feature["properties"]['name'];
-
-         array_push($country, $temp); 
+         array_push($border, $feature); 
     };
-
-    // Use the usort feature to compare items in the country array to put them in alphabetic order
-    usort($country, function ($item1, $item2) {
-
-        // Compare each name against one another and order alphabetically
-         return $item1['name'] <=> $item2['name'];
-    });
-
+    // Assign the output variable properties to relevant data
      $output['status']['code'] = "200";
      $output['status']['name'] = "ok";
      $output['status']['description'] = "success";
      $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-     $output['data'] = $country;
+     $output['data'] = $border;
  
      header('Content-Type: application/json; charset=UTF-8');
 
